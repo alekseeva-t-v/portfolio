60fe81dc5690b8ac464b1fa35a36791df575fb67
Tatyana Alekseeva <alekseeva.tatiana.vi@gmail.com>
2023-07-25 13:47:12 +0300

    feat: Implemented page About translation

--- src/js/modules/lang.js
+++ src/js/modules/lang.js
@@ -1,62 +1,433 @@
 export const langArr = {
   'page-main': {
-    'ru': 'Портфолио',
-    'en': 'Portfolio',
+    ru: 'Портфолио',
+    en: 'Portfolio',
   },
   'page-about': {
-    'ru': 'Обо мне',
-    'en': 'About me',
+    ru: 'Обо мне',
+    en: 'About me',
   },
-  'portfolio': {
-    'ru': 'Портфолио',
-    'en': 'Portfolio',
+  portfolio: {
+    ru: 'Портфолио',
+    en: 'Portfolio',
   },
-  'progects': {
-    'ru': 'Проекты',
-    'en': 'Projects',
+  progects: {
+    ru: 'Проекты',
+    en: 'Projects',
   },
-  'contacts': {
-    'ru': 'Контакты',
-    'en': 'Contacts',
+  contacts: {
+    ru: 'Контакты',
+    en: 'Contacts',
   },
-  'hello': {
-    'ru': 'Привет, меня зовут',
-    'en': 'Hi, my name is',
+  about: {
+    ru: 'Обо мне',
+    en: 'About me',
   },
-  'name': {
-    'ru': 'Татьяна',
-    'en': 'Tatyana',
+  skills: {
+    ru: 'Навыки',
+    en: 'Skills',
   },
-  'specialization-first': {
-    'ru': 'Я Junior',
-    'en': 'I am a Junior',
+  experience: {
+    ru: 'Опыт',
+    en: 'Experience',
   },
-  'specialization-second': {
-    'ru': 'Frontend Разработчик',
-    'en': 'Frontend Developer',
+  hello: {
+    ru: `Привет, меня зовут <em>Татьяна</em>`,
+    en: `Hi, my name is <em>Tatyana</em>`,
   },
-  'specialization-third': {
-    'ru': 'из Тулы',
-    'en': 'from Tula',
+  specialization: {
+    ru: `Я Junior <em>Frontend Разработчик</em> из Тулы`,
+    en: `I am a junior <em>Frontend Developer</em> from Tula`,
   },
-  'cv': {
-    'ru': 'Скачать резюме',
-    'en': 'DownLoad CV',
+  cv: {
+    ru: 'Скачать резюме',
+    en: 'DownLoad CV',
   },
-  'about': {
-    'ru': 'Обо мне',
-    'en': 'About me',
+  name: {
+    ru: 'Меня зовут Татьяна',
+    en: 'My name is Tatyana',
   },
-  'skills': {
-    'ru': 'Навыки',
-    'en': 'Skills',
+  aboutText: {
+    ru: `<p>
+    Я Frontend разработчик с опытом проектов в сфере более года. Прошла
+    курсы <b>Нетологии</b> <span class="selection-text">"JavaScript-программирование для начинающих"</span
+    >, <span class="selection-text">"React для JS-разработчиков"</span>, а также курс <b>Rolling Scopes School</b> <span class="selection-text">"JavaScript/Front-end"</span>. Имею опыт верстки и разработки Web приложений.
+  </p>
+  <p>
+    Способна ставить цели и работать на результат. Умею расставлять приоритеты и выполнять задачи в заданные сроки. Хорошо работаю в команде. Готова браться за новые проекты и доводить дело до конца. Мне нравится участвовать в процессах обучения, адаптации персонала и самой узнавать новое, развиваться.
+  </p>`,
+    en: `
+    <p>
+      I am a Frontend developer with over a year of experience in projects in the field. Passed courses <b>Netology</b> <span class="selection-text">"JavaScript-programming for beginners"</span>, <span class="selection-text">"React for JS developers"</span>, course <b>Rolling Scopes School</b> <span class="selection-text">"JavaScript/Front-end"</span>. I have experience in layout and development of Web applications.
+    </p>
+    <p>
+      Able to set goals and work towards results. I know how to prioritize and complete tasks on time. I work well in a team. Ready to take on new projects and get things done. I like to participate in the processes of training, staff adaptation and to learn new things and develop myself.
+    </p>`,
   },
-  'experience': {
-    'ru': 'Опыт',
-    'en': 'Experience',
+  layout: {
+    ru: `
+    <h4 class="about__skills-title">Верстка</h4>
+    <p class="about__skills-text">
+      Уверенные знания HTML5 / CSS3, адаптивная и кроссбраузерная верстка, Grid, FlexBox, SVG.
+    </p>
+    `,
+    en: `
+    <h4 class="about__skills-title">Layout</h4>
+    <p class="about__skills-text">
+      Strong knowledge of HTML5 / CSS3, adaptive and cross-browser layout, Grid, FlexBox, SVG.
+    </p>
+    `,
   },
-  'copy': {
-    'ru': 'Алексеева Татьяна',
-    'en': 'Alekseeva Tatyana',
+  development: {
+    ru: `
+    <h4 class="about__skills-title">JavaScript разработка</h4>
+    <p class="about__skills-text">
+      Хорошее знание нативного JavaScript и популярных библиотек Bootstrap / JQuerry.
+    </p>
+    `,
+    en: `
+    <h4 class="about__skills-title">JavaScript development</h4>
+    <p class="about__skills-text">
+      Good knowledge of native JavaScript and popular Bootstrap/JQuery libraries.
+    </p>
+    `,
+  },
+  react: {
+    ru: `
+    <h4 class="about__skills-title">React разработка</h4>
+    <p class="about__skills-text">
+      Знание библиотеки React. Опыт работы с Redux и Redux-toolkit.
+    </p>
+    `,
+    en: `
+    <h4 class="about__skills-title">React development</h4>
+    <p class="about__skills-text">
+      Knowledge of the React library. Experience with Redux and Redux-toolkit.
+    </p>
+    `,
+  },
+  cms: {
+    ru: `
+    <h4 class="about__skills-title">Работа с CMS</h4>
+    <p class="about__skills-text">
+      Опыт работы с Wordpress. Базовые знания PHP.
+    </p>
+    `,
+    en: `
+    <h4 class="about__skills-title">Working with CMS</h4>
+    <p class="about__skills-text">
+      Experience with Wordpress. Basic knowledge of PHP.
+    </p>
+    `,
+  },
+  workTitle: {
+    ru: 'Работа и обучение',
+    en: 'Work and study',
+  },
+  education: {
+    ru: 'Образование',
+    en: 'Education',
+  },
+  educationItem: {
+    ru: `
+    <h5 class="experience__item-title">
+      Всероссийский заочный Финансово-экономический институт
+    </h5>
+    <p class="experience__item-location">
+      Диплом с отличием | Тула (2003 - 2009)
+    </p>`,
+    en: `
+    <h5 class="experience__item-title">
+      Institute of Finance and Economics
+    </h5>
+    <p class="experience__item-location">
+      Diploma with honors | Tula (2003 - 2009)
+    </p>`,
+  },
+  educationItemDesc: {
+    ru: `
+    <p><b>Направление</b>: Маркетинг</p>
+    <p><b>Специализация</b>: Коммерческий маркетинг</p>
+    <p><b>Квалификация</b>: Маркетолог</p>`,
+    en: `
+    <p><b>Direction</b>: Marketing</p>
+    <p><b>Specialization</b>: Commercial Marketing</p>
+    <p><b>Qualification</b>: Marketer</p>`,
+  },
+  educationItemLink: {
+    ru: 'Диплом о высшем образовании',
+    en: 'Diploma of Higher Education',
+  },
+  work: {
+    ru: 'Опыт работы',
+    en: 'Experience',
+  },
+  workItemMegafon: {
+    ru: `
+    <h5 class="experience__item-title">ПАО «МегаФон»</h5>
+    <p class="experience__item-location">
+      Июнь 2018 – Настоящее время
+    </p>`,
+    en: `
+    <h5 class="experience__item-title">«MegaFon»</h5>
+    <p class="experience__item-location">
+      June 2018 - Present
+    </p>`,
+  },
+  workItemMegafonDesc: {
+    ru: `
+    <p>
+      <b>Должность</b>: Специалист отдела Федерального телемаркетинга
+    </p>
+    <p>
+      <b>Обязанности</b>: Продвижение услуг, тарифов и опций компании.
+    </p>`,
+    en: `
+    <p>
+      <b>Job title</b>: Federal Telemarketing Specialist
+    </p>
+    <p>
+      <b>Responsibilities</b>: Promotion of services, tariffs and options of the company.
+    </p>`,
+  },
+  workItemAncor: {
+    ru: `
+    <h5 class="experience__item-title">ООО «АНКОР кадровые решения»</h5>
+    <p class="experience__item-location">
+      Июнь 2009 – 2018
+    </p>`,
+    en: `
+    <h5 class="experience__item-title">"ANKOR HR Solutions"</h5>
+    <p class="experience__item-location">
+      June 2009 – 2018
+    </p>`,
+  },
+  workItemAncorDesc: {
+    ru: `
+    <p>
+      <b>Должность</b>: Специалист по работе с Клиентами, Наставник
+    </p>
+    <p>
+      <b>Обязанности</b>: Консультация юридических и физических лиц по услугам и тарифам компании. Работа с базой данных клиентов. Наставничество и адаптация новых сотрудников, помощьсотрудникам в решении сложных вопросов.
+    </p>`,
+    en: `
+    <p>
+      <b>Job title</b>: Client Relations Specialist, Mentor
+    </p>
+    <p>
+      <b>Responsibilities</b>: Consultation of legal entities and individuals on the services and tariffs of the company. Working with customer database. Mentoring and adaptation of new employees, assistance to employees in solving complex issues.
+    </p>`,
+  },
+  coursesTitle: {
+    ru: 'Курсы',
+    en: 'Courses',
+  },
+  coursesItemNetologyReact: {
+    ru: `
+    <h5 class="experience__item-title">Нетология</h5>
+    <p class="experience__item-location">
+      Январь 2023 - Март 2023
+    </p>`,
+    en: `
+    <h5 class="experience__item-title">Netology</h5>
+    <p class="experience__item-location">
+      January 2023 - March 2023
+    </p>`,
+  },
+  coursesItemNetologyReactDesc: {
+    ru: `
+    <p>
+      <b>Курс</b>:
+      <span class="selection-text">React для JS-разработчиков</span>
+    </p>
+    <p>
+      <b>Улучшаемые навыки</b>: Основы работы с библиотекой React • Жизненный цикл и работа с HTTP • Компоненты высшего порядка • Hooks • React Router • Redux • Redux Toolkit
+    </p>`,
+    en: `
+    <p>
+      <b>Course</b>:
+      <span class="selection-text">React for JS Developers</span>
+    </p>
+    <p>
+      <b>Improved Skills</b>: React Library Basics • Lifecycle and HTTP • Higher Order Components • Hooks • React Router • Redux • Redux Toolkit
+    </p>`,
+  },
+  coursesItemNetologyReactLink: {
+    ru: 'Удостоверение о повышении квалификации',
+    en: 'Professional Development Certificate',
+  },
+  coursesItemRSJavaScript0: {
+    ru: `
+    <h5 class="experience__item-title">Rolling Scopes School</h5>
+    <p class="experience__item-location">
+      Декабрь 2022 - Февраль 2023
+    </p>`,
+    en: `
+    <h5 class="experience__item-title">Rolling Scopes School</h5>
+    <p class="experience__item-location">
+      December 2022 - February 2023
+    </p>`,
+  },
+  coursesItemRSJavaScript0Desc: {
+    ru: `
+    <p>
+      <b>Курс</b>:
+      <span class="selection-text">JavaScript/Front-end. Stage 0</span>
+    </p>
+    <p>
+      <b>Улучшаемые навыки</b>: Основы работы с системой контроля версий Git • Chrome Dev Tools • VS Code • Терминалом • HTML • CSS • FlexBox • Адаптивная и мобильная верстка • Figma • Основы JavaScript • DOM API • DOM Events • Структуры данных и алгоритмы
+    </p>`,
+    en: `
+    <p>
+      <b>Course</b>:
+      <span class="selection-text">JavaScript/Front-end. Stage 0</span>
+    </p>
+    <p>
+      <b>Improved Skills</b>: Git Version Control Basics • Chrome Dev Tools • VS Code • Terminal • HTML • CSS • FlexBox • Responsive and Mobile Layout • Figma • JavaScript Fundamentals • DOM API • DOM Events • Data Structures and Algorithms
+    </p>`,
+  },
+  coursesItemRSJavaScript0Link: {
+    ru: 'Сертификат об обучении',
+    en: 'Training certificate',
+  },
+  coursesItemNetologyJS: {
+    ru: `
+    <h5 class="experience__item-title">Нетология</h5>
+    <p class="experience__item-location">
+      Сентябрь 2022 - Июль 2023
+    </p>`,
+    en: `
+    <h5 class="experience__item-title">Netology</h5>
+    <p class="experience__item-location">
+    September 2022 - July 2023
+    </p>`,
+  },
+  coursesItemNetologyJSDesc: {
+    ru: `
+    <p>
+      <b>Курс</b>:
+      <span class="selection-text">JavaScript-программирование для начинающих</span>
+    </p>
+    <p>
+      <b>Улучшаемые навыки</b>: HTML • CSS • Верстка форм • FlexBox • Адаптивная и мобильная верстка • Система контроля версий Git • Основы программирования • Основы JavaScript • Основы разработки интерфейсов • Работа с DOM • Работа с состоянием
+    </p>`,
+    en: `
+    <p>
+      <b>Course</b>:
+      <span class="selection-text">JavaScript programming for beginners</span>
+    </p>
+    <p>
+      <b>Improved Skills</b>: HTML • CSS • Form Layout • FlexBox • Responsive and Mobile Layout • Git Version Control • Programming Fundamentals • JavaScript Fundamentals • Interface Development Fundamentals • Working with the DOM • Working with State
+    </p>`,
+  },
+  coursesItemNetologyJSLink: {
+    ru: 'Диплом профессиональной переподготовки',
+    en: 'Professional Retraining Diploma',
+  },
+  coursesItemNetologyLayout: {
+    ru: `
+    <h5 class="experience__item-title">Нетология</h5>
+    <p class="experience__item-location">
+      Июнь 2022 - Июль 2022
+    </p>`,
+    en: `
+    <h5 class="experience__item-title">Netology</h5>
+    <p class="experience__item-location">
+      June 2022 - July 2022
+    </p>`,
+  },
+  coursesItemNetologyLayoutDesc: {
+    ru: `
+    <p>
+      <b>Курс</b>:
+      <span class="selection-text">Основы верстки сайта</span>
+    </p>
+    <p>
+      <b>Улучшаемые навыки</b>: HTML • CSS • Адаптивная верстка
+    </p>`,
+    en: `
+    <p>
+      <b>Course</b>:
+      <span class="selection-text">Website layout basics</span>
+    </p>
+    <p>
+      <b>Improved Skills</b>: HTML • CSS • Responsive layout
+    </p>`,
+  },
+  useWork: {
+    ru: 'Использую в работе',
+    en: 'Use at work',
+  },
+  skillsHTML5: {
+    ru: 'Умение писать валидный/W3C код • Применение методологии БЭМ • Хорошее понимание семантики HTML',
+    en: 'Ability to write valid/W3C code • Application of BEM methodology • Good understanding of HTML semantics',
+  },
+  skillsCSS3: {
+    ru: 'Кроссбраузерная верстка • Grid • FlexBox • Создание адаптивного и респонсив дизайна • Мобильная верстка',
+    en: 'Cross-browser layout • Grid • FlexBox • Creation of adaptive and responsive design • Mobile layout',
+  },
+  skillsJavaScript: {
+    ru: 'Умение работать с ванильным JavaScript • Базовый опыт работы с DOM • Понимание принципов ООП и их реализация',
+    en: 'Ability to work with vanilla JavaScript • Basic DOM experience • Understanding of OOP principles and their implementation',
+  },
+  skillsTypeScript: {
+    ru: 'Базовые знания TypeScript • Понимание основ и современных концепций',
+    en: 'Basic knowledge of TypeScript • Understanding the basics and modern concepts',
+  },
+  skillsGit: {
+    ru: 'Базовые знания Git • Опыт работы с GitHub • Понимание принципов контроля версий',
+    en: 'Basic knowledge of Git • Experience with GitHub • Understanding version control principles',
+  },
+  skillsGulp: {
+    ru: 'Опыт работы с Gulp для оптимизации проектов и автоматизации сборки',
+    en: 'Experience with Gulp for project optimization and build automation',
+  },
+  skillsWebpack: {
+    ru: 'Опыт работы с системой сборки Webpack • Умение работать на уровне построения конфига',
+    en: 'Experience with the Webpack build system • Ability to work at the config building level',
+  },
+  skillsNodejs: {
+    ru: 'Опыт работы с Node.js (npm) • Базовые навыки: установить / обновить / собрать проект',
+    en: 'Experience with Node.js (npm) • Basic skills: install / update / build project',
+  },
+  skillsReact: {
+    ru: 'Базовое знание библиотеки React (Жизненный цикл компонента, хуки, оптимизация...) • Опыт применения',
+    en: 'Basic knowledge of the React library (Component life cycle, hooks, optimization...) • Application experience',
+  },
+  skillsRedux: {
+    ru: 'Понимание идеологии Redux • Опыт работы с Redux и Redux-toolkit',
+    en: 'Understanding the Redux ideology • Experience with Redux and Redux-toolkit',
+  },
+  skillsBootstrap: {
+    ru: 'Навык работы с Bootstrap 5 • Опыт быстрого создания и настройки сайтов, ориентированных на мобильные устройства',
+    en: 'Proficiency with Bootstrap 5 • Experience in quickly building and customizing mobile-friendly websites',
+  },
+  skillsjQuery: {
+    ru: 'Базовые знания jQuery • При необходимости ускорение с её помощью режима работы',
+    en: 'Basic knowledge of jQuery • If necessary, speed up the mode of operation with its help',
+  },
+  skillsSass: {
+    ru: 'Опыт работы с CSS препроцессором SASS • Использование языка SCSS для написания стилей',
+    en: 'Experience with the SASS CSS preprocessor • Using the SCSS language to write styles',
+  },
+  skillsFigma: {
+    ru: 'Базовые навыки работы с Figma • Умение верстать компоненты и страницы по макетам из Figma',
+    en: 'Basic skills of working with Figma • Ability to layout components and pages based on layouts from Figma',
+  },
+  skillsPhotoshop: {
+    ru: 'Базовое знание функционала • Умение работать с макетами Photoshop',
+    en: 'Basic knowledge of functionality • Ability to work with Photoshop layouts',
+  },
+  skillsPHP: {
+    ru: 'Базовые знания о PHP • Владение инструментами разработки',
+    en: 'Basic knowledge of PHP • Knowledge of development tools',
+  },
+  skillsWordpress: {
+    ru: 'Понимание основ работы с серверами, хостингами • Опыт верстки под Wordpress',
+    en: 'Understanding the basics of working with servers, hosting • Experience in layout for Wordpress',
+  },
+  copy: {
+    ru: 'Алексеева Татьяна',
+    en: 'Alekseeva Tatyana',
   },
 };
