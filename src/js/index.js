92a20061f611ba18a0e7fd0e3ba63d7df9a4f943
Tatyana Alekseeva <alekseeva.tatiana.vi@gmail.com>
2023-08-18 11:40:26 +0300

    docs: update readme with additional information

--- src/js/index.js
+++ src/js/index.js
@@ -4,6 +4,8 @@ import changeColorSheme from './modules/change-color-sheme';
 import showHamburger from './modules/hamburger';
 import { changeLinkActiveForHome } from './modules/change-link-active';
 
+const portfolioCardList = document.querySelectorAll('.portfolio__card');
+
 // changeLang('page-main');
 setTimeout(showBody, 0);
 changeColorSheme();
@@ -14,3 +16,14 @@ window.addEventListener('scroll', () => {
   changeLinkActiveForHome();
 });
 
+portfolioCardList.forEach((card) => {
+  card.addEventListener('click', () => {
+    portfolioCardList.forEach((cardElem) => {
+      if (card !== cardElem) {
+        cardElem.classList.remove('portfolio__card--active');
+      }
+    });
+
+    card.classList.toggle('portfolio__card--active');
+  });
+});
