b8b2d397ad1cde73c54cc1cff42ede0d931c513a
Tatyana Alekseeva <alekseeva.tatiana.vi@gmail.com>
2023-10-02 14:11:30 +0300

    feat: add project description Css Bayan

---
+++ src/js/games.js
@@ -0,0 +1,9 @@
+// import changeLang from './modules/change-lang';
+import showBody from './modules/show-body';
+import changeColorSheme from './modules/change-color-sheme';
+import showHamburger from './modules/hamburger';
+
+// changeLang('page-about');
+setTimeout(showBody, 0);
+changeColorSheme();
+showHamburger();
--- webpack.config.js
+++ webpack.config.js
@@ -4,8 +4,10 @@ const config = {
     index: './src/js/index.js',
     about: './src/js/about.js',
     projects: './src/js/projects.js',
+    games: './src/js/games.js',
     contacts: './src/js/contacts.js',
     momentum: './src/js/momentum.js',
+    bayan: './src/js/bayan.js',
   },
   output: {
     filename: '[name].bundle.js',
